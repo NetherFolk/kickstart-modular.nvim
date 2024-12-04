ba7e212987cd4775c8ed546de09a5236f1ed4f0b
NetherFolk <nazrinsp.51@gmail.com>
2024-12-05 03:07:56 +0800

    Added autopairs to the config

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -55,7 +55,8 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+
+  require 'kickstart.plugins.autopairs',
 
   require 'kickstart.plugins.neo-tree',
 
